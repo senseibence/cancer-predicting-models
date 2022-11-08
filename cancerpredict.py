3966bc283cf3fb12780c28fdb82d3a4366f70594
Bence Lukacsy <bence.lukacsy@gmail.com>
2022-11-08 01:25:01 -0500

    working models with high accuracy

--- cancerpredict.py
+++ cancerpredict.py
@@ -1,63 +1,49 @@
 import pandas
 import numpy
 import xgboost as xgb
-'''from sklearn.model_selection import train_test_split '''
+from sklearn.model_selection import train_test_split 
 from sklearn import tree
-'''from sklearn.tree import DecisionTreeClassifier // Classifier will be for no/yes (0/1) when I get medical data'''
+
+''' Classifier will be for no/yes (0/1) when I get medical data'''
+from sklearn.tree import DecisionTreeClassifier 
+
+''' This is for classifiers only '''
+from sklearn.metrics import accuracy_score
+
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import accuracy_score
+
 import matplotlib.pyplot as plt
 
 encode = LabelEncoder()
 
-df = pandas.read_csv('allvariables&p500.csv')
-'''df = pandas.read_csv('old.csv')'''
-
-'''
-print(df)
-print(df.head())
-print(df.shape)
-print(df.columns)
-print(df.isnull().sum())
-'''
+df = pandas.read_csv('testdata.csv')
+'''df = pandas.read_csv('categoricaloutput.csv')'''
 
 inputs = ['Open','High','Low']
-
 X = df[inputs]
 y = df['Close']
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+
 '''dtree = DecisionTreeClassifier()''' 
 dtree = DecisionTreeRegressor()
-dtree = dtree.fit(X.values, y)
+dtree = dtree.fit(X_train,y_train) 
+'''regularDTprediction = dtree.predict(X_test)'''
 
-tree.plot_tree(dtree, feature_names=inputs)
+xgbtree = xgb.XGBRegressor()
+xgbtree = xgbtree.fit(X_train,y_train)
+'''XGBoostDTprediction = xgbtree.predict(X_test)'''
 
-''' 3657.1,	3757.89, 3647.42, 3752.75 '''
-print(dtree.predict([[3830, 4120, 3663.92,]]))
+''' R^2 is 0.9999... for both models '''
+print("Accuracy of Model:",dtree.score(X_test, y_test))
+print("Accuracy of Model:",xgbtree.score(X_test, y_test))
 
+''' 
+Next Goals | Primary :
+Try out sklearn random forest algorithm, tensorflow neural network, 
+the categorial data, and adding parameters to XGBoost models
 
-'''
-train_X = X[:80]
-train_y = y[:80]
-
-test_X = X[80:]
-test_y = y[80:]
-
-train_X, test_X, train_y, test_y = train_test_split(X, y, test_size = 0.2, random_state = 0)
-
-train_y = encode.fit_transform(train_y)
-test_y = encode.fit_transform(test_y)
-
-dTree_clf = DecisionTreeClassifier()
-xgb_classifier = xgb.XGBClassifier()
-
-dTree_clf.fit(train_X,train_y)
-xgb_classifier.fit(train_X,train_y)
-
-pred2_y = dTree_clf.predict(test_X)
-predictions = xgb_classifier.predict(test_X)
-
-print("Accuracy of Model::",accuracy_score(test_y,pred2_y))
-print("Accuracy of Model::",accuracy_score(test_y,predictions))
+Next Goals | Secondary :
+Try logistic regression, K-neighbors, guassianNB, support vector machines, etc (all in bookmarked links)
 '''
