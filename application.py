2a74f2d50a16b530b55cbb4e29dda9468bb78051
Bence Lukacsy <bence.lukacsy@gmail.com>
2023-01-11 22:22:46 -0500

    website changes

--- application.py
+++ application.py
@@ -3,8 +3,8 @@ import joblib
 
 application = Flask(__name__)
 
-file = open("model.pkl", "rb")
-model = joblib.load(file)
+model = joblib.load('XGBmodel.pkl')
+scaler = joblib.load('scaler.pkl')
 
 @application.route("/")
 def home():
@@ -24,14 +24,32 @@ def predict():
     features = request.form.to_dict()
     features = list(features.values())
     features = list(map(int, features))
+
+    # bmi calculation
+    feet = features[1]
+    inches = features[2]
+    converted_inches = inches / 12
+    feet += converted_inches
+    meters = feet / 3.281
+    pounds = features[3]
+    kg = pounds / 2.205
+
+    bmi = 27.282130617023647
+    if (meters != 0):
+        bmi = kg / (pow(meters,2))
+        
+    del features[1:4]
+    features.insert(14, bmi)
+
+    features = scaler.transform([features])
+
+    prediction_result = model.predict(features)
+    prediction_probability = model.predict_proba(features)
+
+    if (prediction_result == 0):
+        return render_template('index.html', prediction = 'The model is '+str((prediction_probability[0][0])*100)+'% confident that you DO NOT have colon cancer. Please remember that this is not a diagnosis.')
+    if (prediction_result == 1):
+        return render_template('index.html', prediction = 'The model is '+str((prediction_probability[0][1])*100)+'% confident that you should get a screening for colon cancer. Please remember that this is not a diagnosis.')
     
-    prediction = model.predict([features])
-
-    result = 'Failed'
-    if (prediction == 0): result = 'No'
-    if (prediction == 1): result = 'Yes'
-
-    return render_template('index.html', prediction = 'Colon Cancer Prediction: '+result)
-
 if __name__ == '__main__':
-    application.run()
+    application.run(debug=True)
