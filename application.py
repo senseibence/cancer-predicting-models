eace87a4e5e8b26024f7f2af5c9f294979f57f0f
Bence Lukacsy <bence.lukacsy@gmail.com>
2023-01-19 02:22:01 -0500

    made proper API

--- application.py
+++ application.py
@@ -1,9 +1,45 @@
 from flask import Flask, request, render_template
 import joblib
-import keras
-import numpy
+# import keras
+# import numpy
+import json
 
-application = Flask(__name__)
+keys = [
+
+    'age',
+    'height1',
+    'height2',
+    'weight',
+    'sex',
+    'race7',
+    'educat',
+    'marital',
+    'occupat',
+    'cig_stat',
+    'cig_years',
+    'cigpd_f',
+    'cigar',
+    'pipe',
+    'fh_cancer',
+    'colo_fh',
+    'colo_fh_cnt',
+    'asppd',
+    'ibuppd',
+    'arthrit_f',
+    'bronchit_f',
+    'colon_comorbidity',
+    'diabetes_f',
+    'divertic_f',
+    'emphys_f',
+    'gallblad_f',
+    'hearta_f',
+    'hyperten_f',
+    'liver_comorbidity',
+    'osteopor_f',
+    'polyps_f',
+    'stroke_f'
+
+]
 
 XGBmodel = joblib.load('XGBmodel.pkl')
 XGBscaler = joblib.load('XGBscaler.pkl')
@@ -12,6 +48,23 @@ XGBscaler = joblib.load('XGBscaler.pkl')
 Neuralmodel = keras.models.load_model('C:\\Users\\bence\\Documents\\python programs\\cancer-predicting-models')
 Neuralscaler = joblib.load('Neuralscaler.pkl')
 '''
+
+def calculateBMI(features):
+    feet = features[1]
+    inches = features[2]
+    converted_inches = inches / 12
+    feet += converted_inches
+
+    meters = feet / 3.281
+    if (meters == 0): return 27.282130617023647 # division by zero if unchecked
+
+    pounds = features[3]
+    kg = pounds / 2.205
+    bmi = kg / (pow(meters,2)) # division
+
+    return bmi
+
+application = Flask(__name__)
 
 @application.route("/")
 def home():
@@ -28,42 +81,52 @@ def about():
 @application.route('/predict', methods=['POST'])
 def predict():
 
-    features = request.form.to_dict()
+    payloadType = request.headers.get('Content-Type')
+    if (payloadType == 'application/json'):
 
-    if (len(features) == 32):
-        features = list(features.values())
-        features = list(map(int, features))
+        data = request.get_json() 
+        userKeys = []
 
-        # bmi calculation
-        feet = features[1]
-        inches = features[2]
-        converted_inches = inches / 12
-        feet += converted_inches
-        meters = feet / 3.281
-        pounds = features[3]
-        kg = pounds / 2.205
-
-        bmi = 27.282130617023647
-        if (meters != 0):
-            bmi = kg / (pow(meters,2))
-            
-        del features[1:4]
-        features.insert(14, bmi)
-        print(features)
-
-        features = XGBscaler.transform([features])
-
-        prediction_result = XGBmodel.predict(features)
-        prediction_probability = XGBmodel.predict_proba(features)
-
-        if (prediction_result == 0):
-            return render_template('index.html', prediction = 'The model is '+str((prediction_probability[0][0])*100)+'% confident that you DO NOT have colon cancer. Please remember that this is not a diagnosis.')
-            
-        if (prediction_result == 1):
-            return render_template('index.html', prediction = 'The model is '+str((prediction_probability[0][1])*100)+'% confident that you should get a screening for colon cancer. Please remember that this is not a diagnosis.')
+        for key in data: userKeys.append(key)
         
-    else:
-        return render_template('index.html', prediction = 'Failed')
+        if (userKeys == keys):
+
+            features = []
+            for value in data.values():
+                try: float(value)
+                except: return 'Payload values not of type int or float'
+                features.append(float(value))
+                
+            bmi = calculateBMI(features)
+            del features[1:4]
+            features.insert(14, bmi)
+
+            features = XGBscaler.transform([features])
+            prediction = XGBmodel.predict(features)
+            probability = XGBmodel.predict_proba(features)
+
+            if (prediction == 0): cancerPrediction = 'no'
+            elif (prediction == 1): cancerPrediction = 'yes'
+            if (prediction == 0): predictionProbability = str(probability[0][0]*100)+'%'
+            elif (prediction == 1): predictionProbability = str(probability[0][1]*100)+'%'
+            if (prediction == 0): recommendation = 'Safe to forgo screening'
+            elif (prediction == 1): recommendation = 'Screening highly recommended'
+
+            package = {
+                'statusCode': 200,
+                'results' : {
+                    'prediction' : cancerPrediction,
+                    'probability' : predictionProbability,
+                    'recommendation' : recommendation
+                }
+            }
+
+            package = json.dumps(package, indent=4)
+            return package 
+        
+        else: return 'Payload keys incorrect'
+    
+    else: return 'Payload not of type JSON'
     
 if __name__ == '__main__':
     application.run(debug=True)
